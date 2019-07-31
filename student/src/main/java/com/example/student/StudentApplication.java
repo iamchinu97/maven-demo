e68ab537a126534d94bda01300affe6c30cac9c8
iamchinu97 <shimpichinmay@gmail.com>
2019-07-31 14:11:50 +0530

    new student added for gurpal singh

--- student/src/main/java/com/example/student/StudentApplication.java
+++ student/src/main/java/com/example/student/StudentApplication.java
@@ -2,15 +2,16 @@ package com.example.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 @SpringBootApplication
-public class StudentApplication {
+public class StudentApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(StudentApplication.class, args);
 	}
 	@Bean
